425bba3a5e7a9017f1f1e2a88fec4e99681364b0
MIbrahim9276 <m.ibrahm9276@gmail.com>
2025-07-28 19:59:27 +0200

    Done - Missing UI

--- app/Http/Controllers/JobListingController.php
+++ app/Http/Controllers/JobListingController.php
@@ -12,7 +12,12 @@ class JobListingController extends Controller
      */
     public function index()
     {
-        //
+        if (auth()->user()->cannot('viewAny')) {
+            abort(403);
+        }
+
+        $listings = JobListing::all();
+        return view("joblisting.index", compact("listings"));
     }
 
     /**
@@ -20,7 +25,11 @@ class JobListingController extends Controller
      */
     public function create()
     {
-        //
+        if (auth()->user()->cannot('create')) {
+            abort(403);
+        }
+
+        return view("joblisting.create");
     }
 
     /**
@@ -28,38 +37,79 @@ class JobListingController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        if (auth()->user()->cannot('create')) {
+            abort(403);
+        }
+
+        $job = JobListing::create([
+            "client_id" => 3,
+            "title" => $request->input("title"),
+            "desc" => $request->input("desc"),
+            "budget" => $request->input("budget")
+        ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(JobListing $jobListing)
+    public function show(int $jobListing)
     {
-        //
+        $listing = JobListing::find($jobListing);
+
+        if (auth()->user()->cannot('view', $listing)) {
+            abort(403);
+        }
+
+        return view("joblisting.show", compact("listing"));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(JobListing $jobListing)
+    public function edit(int $jobListing)
     {
-        //
+        $listing = JobListing::find($jobListing);
+
+        if (auth()->user()->cannot('update')) {
+            abort(403);
+        }
+
+        return view("joblisting.edit", compact("listing"));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, JobListing $jobListing)
+    public function update(Request $request, int $jobListing)
     {
-        //
+        $listing = JobListing::find($jobListing);
+
+        if (auth()->user()->cannot('update', $listing)) {
+            abort(403);
+        }
+
+        $listing->update([
+            "title" => $request->input("title"),
+            "desc" => $request->input("desc"),
+            "budget" => $request->input("budget")
+        ]);
+
+        return $this->show($jobListing);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(JobListing $jobListing)
+    public function destroy(int $jobListing)
     {
-        //
+        $listing = JobListing::find($jobListing);
+
+        if (auth()->user()->cannot('delete')) {
+            abort(403);
+        }
+
+        $listing->delete();
+
+        return $this->index();
     }
 }
---
+++ resources/views/joblisting/show.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Job Listings - FreelanceBoard</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+        <!-- Styles / Scripts -->
+        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @endif
+    </head>
+    <body>
+      <div>{{ $listing->title }}</div>
+      <div>{{ $listing->desc }}</div>
+      <div>{{ $listing->budget }}</div>
+
+      <a href="{{ route("jobs.edit", $listing) }}">Edit</a>
+    </body>
+</html>
