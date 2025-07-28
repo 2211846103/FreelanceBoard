c5e8a902349c3b1c88390137fa222c5434e1814c
أوسم العباني <awsamalabani1168@gmail.com>
2025-07-28 19:24:49 +0200

    Added Contract componants

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\ContractController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('contracts', ContractController::class);
