a81358ef052a1a9fea0b76c1941974de0b81d8a1
iTacHi-RS <mrxly12345@gmail.com>
2025-07-29 02:33:00 +0200

    itachiz

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,7 @@
 
 require_once 'Contract.php';
 require_once 'jobs.php';
-<<<<<<< Updated upstream
 require_once 'User.php';
 
 require_once 'demands.php';
-=======
-require_once 'proposal.php';
 require_once 'review.php';
->>>>>>> Stashed changes
